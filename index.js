8b642e5dcb4c59c0ac70787a7dba337004472a4c
willygs <willyganjarsaputra@gmail.com>
2016-07-02 13:19:57 +0700

    Listen

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ io.on('connection',function(socket){
 	});
 });
 
-http.listen(3000, function(){
-	console.log('Listening on : 3000');
+http.listen(process.env.PORT || 3000, function(){
+	console.log('Listening on : heroku');
 });
